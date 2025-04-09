637b283dbdf1947ce9022947d9c86d8dfe8d6368
Serkanmus <44236696+Serkanmus@users.noreply.github.com>
2025-04-09 21:50:00 +0300

    Add files via upload

--- serving_rag.py
+++ serving_rag.py
@@ -96,4 +96,4 @@ def predict(payload: QueryRequest):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8145)
+    uvicorn.run(app, host="0.0.0.0", port=8147)
--- serving_rag_2.py
+++ serving_rag_2.py
@@ -205,4 +205,4 @@ def predict(payload: QueryRequest):
 # Main
 #########################################
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8145)
+    uvicorn.run(app, host="0.0.0.0", port=8147)
