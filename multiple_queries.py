8466e5a9764934b1970404664e758554bf857cfd
Serkanmus <44236696+Serkanmus@users.noreply.github.com>
2025-04-12 19:41:25 +0300

    Add files via upload

--- multiple_queries.py
+++ multiple_queries.py
@@ -28,7 +28,9 @@ def get_my_ip():
 
 # Use the dynamically determined IP in your URL:
 SERVER_IP = get_my_ip()
-SERVER_URL = f"http://{SERVER_IP}:8100/rag"
+# SERVER_URL = f"http://{SERVER_IP}:8100/rag"
+SERVER_URL = f"http://{SERVER_IP}:8147/rag"
+
 
 
 def send_request(payload):
